074583f9638ca5c7de9c9959ca2b2396fd432a7b
Prashanth <noothpallyprashanth444@gmail.com>
2022-08-22 23:49:53 +0530

    Added details

--- src/App.js
+++ src/App.js
@@ -5,6 +5,8 @@ function App() {
   return (
     <div className="App">
      <h1>Continuous Integration Introduction</h1>
+     <h1>Introduction of Local repository to Global repository</h1>
+     <h1>Introduction of local repository to netlify</h1>
 
 
 
