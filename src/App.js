ea3e780a5f5ac530d71fddaa85fcc25b75d2f670
Prashanth <noothpallyprashanth444@gmail.com>
2022-08-23 14:43:12 +0530

    details added

--- src/App.js
+++ src/App.js
@@ -6,6 +6,8 @@ function App() {
      <h1>Continuous Integration Introduction</h1>
      <h1>Introduction of Local repository to Global repository</h1>
      <h1>Introduction of local repository to netlify</h1>
+     <h1>Introduction of global repository to netlify</h1>
+
 
 
 
